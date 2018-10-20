d17b7eb846f1a60d398314fcf7201de66d3b79aa
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-20 17:48:50 -0400

    Initial Commit - Dummy Cookie Sender 1

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+const cookieParser = require('cookie-parser')
+const serveStatic = require('serve-static')
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, partner_1_tracking_id, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+  next();
+});
+
+app.use('/', (req, res, next) => {
+	console.log("Intercepting Request, Logging Cookies: ")
+	console.log(req.cookies)
+	if (!req.cookies['partner_1_tracking_id']) {
+		console.log('Processed Request - User Does Not Have Cookie.')
+		const uniqueID = uuidv4();
+		res.setHeader('Set-Cookie', [`partner_1_tracking_id=${uniqueID}`]);
+	}
+	next();
+});
+
+app.use('/public', serveStatic(path.join(__dirname, '/public')))
+
+app.get('*', (req, res) => {
+	console.log("Catch-All Handler")
+	res.status(200).send("All Clear Chief.")
+});
+
+
+app.use(function(err, req, res, next) {
+	console.log(err)
+	res.status(err.status || 500).send();
+});
+
+
+const port = process.env.PORT || 7000;
+
+app.listen(port);
+
+console.log(`Audience Service Host listening on ${port}`);
---
+++ public/cookieBait.js
@@ -0,0 +1,2 @@
+const myGlobalVariable = 'tockus'
+console.log("Script Is Out There, Somewhere, In The Great Unknown.")
