c807f8d4f4f62f959f6850f4ee5c8e360f9609c7
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-22 16:09:43 -0400

    Removed wildcard allow-access-origin header to allow withCredential xhr requests

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://cookie-sync-audience-service.herokuapp.com/");
   res.header("Access-Control-Allow-Headers", "Origin, partner_1_tracking_id, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
   res.header("Access-Control-Allow-Credentials", true);
